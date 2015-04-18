818d732064a918704a29be1c921a9611e9abc936
Ryan Cole <ryan@rycole.com>
2015-04-18 18:18:27 -0500

    starting to reverse react code

---
+++ replays-web/src/app.jsx
@@ -0,0 +1,28 @@
+import Router from 'react-router';
+import AuthenticatedApp from './components/AuthenticatedApp'
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Home from './components/Home';
+import React from 'react';
+import RouterContainer from './services/RouterContainer'
+import LoginActions from './actions/LoginActions'
+
+var routes = (
+  <Route handler={AuthenticatedApp}>
+    <Route name="login" handler={Login}/>
+    <Route name="signup" handler={Signup}/>
+    <Route name="home" path="/" handler={Home}/>
+  </Route>
+);
+
+var router = Router.create({routes});
+RouterContainer.set(router);
+
+let jwt = localStorage.getItem('jwt');
+if (jwt) {
+  LoginActions.loginUser(jwt);
+}
+
+router.run(function (Handler) {
+  React.render(<Handler />, document.getElementById('content'));
+});
