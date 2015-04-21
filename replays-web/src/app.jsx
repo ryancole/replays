5680b5b541a86f99a5e70fa9e2e360dabba0cd2b
Ryan Cole <ryan@rycole.com>
2015-04-21 18:45:54 -0500

    reduce to simple

--- replays-web/src/app.jsx
+++ replays-web/src/app.jsx
@@ -1,33 +1,7 @@
 import React from 'react';
-import ReactRouter, { Route } from 'react-router';
-import Home from './components/Home';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import LoginActions from './actions/LoginActions';
-import RouterContainer from './services/RouterContainer';
-import AuthenticatedApp from './components/AuthenticatedApp';
+import Replay from 'components/Replay';
 
-// route handlers
-var routes = (
-  <Route handler={AuthenticatedApp}>
-    <Route name="login" handler={Login}/>
-    <Route name="signup" handler={Signup}/>
-    <Route name="home" path="/" handler={Home}/>
-  </Route>
+React.render(
+  <Replay />,
+  document.getElementById('container')
 );
-
-// initialize react router
-var router = ReactRouter.create({routes});
-
-// whatever the fk this is
-RouterContainer.set(router);
-
-let jwt = localStorage.getItem('jwt');
-
-if (jwt) {
-  LoginActions.loginUser(jwt);
-}
-
-router.run(function (Handler) {
-  React.render(<Handler />, document.getElementById('content'));
-});
---
+++ replays-web/src/components/Replay.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FluxComponent from 'flummox/component';
+
+class Replay extends React.Component {
+  render() {
+    return (
+      <FluxComponent>
+        <h1>Hello!</h1>
+      </FluxComponent>
+    );
+  }
+}
+
+export default Replay;
