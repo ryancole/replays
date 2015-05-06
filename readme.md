75da978d7b30cb0428adeef4b63aef362bceb670
Ryan Cole <ryan@rycole.com>
2015-05-06 17:50:19 -0500

    add token code

---
+++ readme.md
@@ -0,0 +1,3 @@
+curl -H "Content-Type: application/json" -X POST -d "{\"username\":\"test\",\"password\":\"test\"}" http://localhost:8080/api/account
+
+curl -H "Content-Type: application/json" -X POST -d "{\"_id\":\"ede0392863d887719aa4335f16001b59\",\"username\":\"test\",\"dateCreated\":1430952382182}" http://localhost:8080/api/session
--- replays-api/src/controller/session.js
+++ replays-api/src/controller/session.js
@@ -1,10 +1,35 @@
+'use strict';
+
+var jwt = require('jsonwebtoken');
 var Boom = require('boom');
 
 /**
  * create a new session
  */
-function create (request, response) {
-  return response(Boom.notImplemented());
+function create (request, reply) {
+
+  // the token payload
+  let payload = {
+    _id: request.payload._id,
+    username: request.payload.username,
+    dateCreated: request.payload.dateCreated
+  };
+
+  // the token options
+  let options = {
+    expiresInMinutes: 60*5
+  };
+
+  // sign the token
+  let token = jwt.sign(payload, 'lol', options);
+
+  // format response
+  let response = {
+    token: token
+  };
+
+  return reply(response);
+
 };
 
 module.exports = [
