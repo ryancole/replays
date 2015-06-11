f923153f437106253a6cd337bcfa55db30541a2f
Ryan Cole <ryan@rycole.com>
2015-06-10 23:19:40 -0500

    add download link

--- replays-web/src/views/AccountReplayView.jsx
+++ replays-web/src/views/AccountReplayView.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import FluxComponent from 'flummox/component';
 import ReplayDetail from '../components/ReplayDetail';
 import SectionNavbar from '../components/SectionNavbar';
+import ReplayDetailNavbar from '../components/ReplayDetailNavbar';
 
 
 class AccountReplayView extends React.Component {
@@ -9,8 +10,9 @@ class AccountReplayView extends React.Component {
   render () {
 
     const replay = this.props.replay;
+    const account = this.props.account;
 
-    if (replay == null) {
+    if (replay == null || account == null) {
       return null;
     }
 
@@ -18,8 +20,9 @@ class AccountReplayView extends React.Component {
       <div>
         <div className="row">
           <div className="col-sm-12">
-            <SectionNavbar
-              label={replay.filename} />
+            <SectionNavbar label={replay.filename}>
+              <ReplayDetailNavbar />
+            </SectionNavbar>
           </div>
         </div>
         <div className="row">
