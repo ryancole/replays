59943e1ce8f9f9994320a7053767d4a66ecd786e
Ryan Cole <ryan@rycole.com>
2015-06-27 12:33:47 -0500

    foo

--- replays-web/src/AppSettings.js
+++ replays-web/src/AppSettings.js
@@ -1,12 +1,10 @@
-import settings from "../settings";
+import settings from '../settings';
 
-// default to development settings
-let output = settings.development;
-
-// optionally set to production
-if (process.env.NODE_ENV == "production") {
-  output = settings.production;
-}
-
-// just export this crap
-export default output;
+export default {
+  get: key => {
+    if (process.env.NODE_ENV == "production") {
+      return settings.production[key];
+    }
+    return settings.development[key];
+  }
+};
