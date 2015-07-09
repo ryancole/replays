1614ced3c0b76289f048d5405756b2524dba79a7
Ryan Cole <ryan@rycole.com>
2015-07-09 18:55:29 -0500

    config

---
+++ replays-web/dank.config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// development settings
+let settings = {
+  "API_ADDR": "http://localhost:8080",
+  "DOWNLOAD_PREFIX": "https://cdn.dank.gg"
+};
+
+if (process.env.NODE_ENV == 'production') {
+  settings = {
+    "API_ADDR": "http://api.dank.gg",
+    "DOWNLOAD_PREFIX": "https://cdn.dank.gg"  
+  };
+}
+
+module.exports = settings;
